b22d8111cc1c03581499b0f196f1c545731ad138
Mayol Lucas <lucas1.mayol@epitech.eu>
2018-07-20 10:36:56 +0200

    adjute game and file my

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ SpriteSheet	rogueLikeSheet((unsigned int *)rogueLikeTiles, rogueLikeTilesLen,
 			       96, 160);
 //global for game
 TBackground	*bg;
+TBackground	*bg2;
 Sprite		*a;
 BColliderMap	*CMap;
 BMap		*bMap;
@@ -33,13 +34,14 @@ bool		jump = false;
 int		dt = 0;
 float		v = 2.;
 int		sx = 0;
+int		bgx = 0;
 vec2f		g_v = {2, 0};
 
 
 //MAP
 
-const u16	O = 1;
-const u16	M = 33;
+const u16	O = 43;
+const u16	M = 24;
 
 u16		mapA[] =
   {
@@ -60,18 +62,22 @@ u16		mapA[] =
 
 bool		engineInit()
 {
-  bg = new TBackground(TBackground::SUB, BgSize_T_512x256, &rogueLikeSheet, TileLayer1, 16, 16);
+  bg = new TBackground(TBackground::SUB, BgSize_T_512x256, &rogueLikeSheet, TileLayer1, 16, 16, 1);
+  bg2 = new TBackground(TBackground::SUB, BgSize_T_512x256, &rogueLikeSheet, TileLayer1, 16, 16, 0);
 
+  BMap	mapBG({16, 16}, {2, 1}, true);
+  
   bMap = new BMap({16, 16}, {2, 1}, true);
-  CMap = new BColliderMap(bMap, {16, 16}, {33});
-  bMap->init(1);
+  CMap = new BColliderMap(bMap, {16, 16}, {M});
+
+  mapBG.init(9);
+  bMap->init(O);
   bMap->setMap(0, mapA);
   bMap->setMap(1, mapA);
-  // for (int x = 0; x < 16; x += 1)
-  //     bMap->set(x, 4, 33, 0);
   bg->addInMapT16(mapA, 16, 16, 0);
   bg->addInMapT16(mapA, 16, 16, 1);
-  // bg->addInMapT16(bMap->getData(), 16, 16, 0);
+  bg2->addInMapT16(mapBG.getData(), 16, 16, 0);
+  bg2->addInMapT16(mapBG.getData(), 16, 16, 1);
   bg->addInMapT16(bMap->getMap(1).data, 16, 16, 1);
 
   a = new Sprite(Sprite::SUB, SpriteSize_16x16, &rogueLikeSheet, 42, Sprite::ABSOLUTE);
@@ -108,9 +114,11 @@ bool		enginePreUpdate()
 
 bool		engineUpdate()
 {
+  bg2->scroll(bgx, 0);
   bg->scroll(sx, 0);
   a->scroll(-sx, 0);
   sx += v;
+  bgx += v / 2 > 0 ? v / 2 : 1;
   if (sx >= 512)
     {
       a->setPos(a->getPos().x - 512, a->getPos().y);
